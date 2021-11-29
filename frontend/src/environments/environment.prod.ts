7dbdeb38a4dbfad495d42e0f5405860baddff3e1
Nguyen Thanh Duc <nthanhduc2000@gmail.com>
2021-11-29 17:09:49 +0800

    add jwt key

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  // apiUrl: 'http://group3-1-i.comp.nus.edu.sg:5436'
-  apiUrl: 'http://localhost:3000'
+  // apiUrl: 'https://group3-1-i.comp.nus.edu.sg:5436',
+  apiUrl: 'http://localhost:3000',
+  JWT_KEY: "ifsgrp3ifs4205"
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -5,5 +5,6 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://group3-1-i.comp.nus.edu.sg:5436'
+  apiUrl: 'http://group3-1-i.comp.nus.edu.sg:5436',
+  JWT_KEY: "ifsgrp3ifs4205"
 };
